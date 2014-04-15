b4bbef8ec77fc1e7cf760b27a0775c29418a8c5f
dmellosnehal23 <snehal.v.dmello@gmail.com>
2014-04-15 03:21:45 -0700

    added support for fetching user info

--- grails-app/controllers/sjsu/cmpe295/controllers/RestController.groovy
+++ grails-app/controllers/sjsu/cmpe295/controllers/RestController.groovy
@@ -8,9 +8,10 @@ class RestController {
 	def restService
 
 	def index() {
+		println("In index()")
 		def errorMessage
 		try{
-			def properties = restService.getData(params)
+			def properties = restService.getProperties(params)
 			if(properties) {
 				
 				if(params.dtype &&  "xml".equals(params.dtype.toLowerCase()))
@@ -26,4 +27,47 @@ class RestController {
 		}
 		render  JSON.parse("{\"error\" : \"" + errorMessage + "\"}") as JSON
 	}
+	
+	def users() {
+		println("In users()")
+		def errorMessage
+		try{
+			def user = restService.getUser(params)
+			if(user){
+				println("User Present")
+				if(params.dtype &&  "xml".equals(params.dtype.toLowerCase()))
+					render user as XML
+				else
+					render user as JSON	
+			}
+			else
+				errorMessage = "No user available"
+		}
+		catch(Exception e){
+			errorMessage = e.getMessage()
+		}
+		render  JSON.parse("{\"error\" : \"" + errorMessage + "\"}") as JSON
+	}
+	
+	def watchlist(){
+		println("In watchlist()")
+		def errorMessage
+		try{
+			def user = restService.addToWatchList(params)
+			if(user){
+				println("User Present")
+				if(params.dtype &&  "xml".equals(params.dtype.toLowerCase()))
+					render user as XML
+				else
+					render user as JSON
+			}
+			else
+				errorMessage = "No user available"
+		}
+		catch(Exception e){
+			errorMessage = e.getMessage()
+		}
+		render  JSON.parse("{\"error\" : \"" + errorMessage + "\"}") as JSON
+	}
+	
 }
--- grails-app/services/sjsu/cmpe295/services/RestService.groovy
+++ grails-app/services/sjsu/cmpe295/services/RestService.groovy
@@ -1,36 +1,72 @@
 package sjsu.cmpe295.services
 
+import org.apache.jasper.compiler.Node.ParamsAction;
+
 import grails.transaction.Transactional
 import sjsu.cmpe295.models.MasterUnSoldProperty
+import sjsu.cmpe295.models.User
 
 @Transactional
 class RestService {
 
-	def getData(params) {
+	def getProperties(params) {
 		def properties
-		
+
 		def offset = 0
 		def max = 10
 		if(params.offset)
 			offset = params.offset
 		if(params.max)
 			max = params.max
-			
-		
-		def filters = [ max : max, offset : offset, sort : "id"]		
-		if(params.city)
-		{
-			def city = params.city
+
+
+		def filters = [ max : max, offset : offset, sort : "id"]
+		if(params.pid){
+			properties = MasterUnSoldProperty.findById(params.pid)
+		}
+		else if(params.city){
 			//masterUnSoldProperty = MasterUnSoldProperty.findByCity(city)
-			properties = MasterUnSoldProperty.findAllByCity(city, filters)
+			properties = MasterUnSoldProperty.findAllByCity(params.city, filters)
 		}
 		else if(params.address) {
-			def address = params.address
-			println(address)
-			properties = MasterUnSoldProperty.findByAddress("\"" + address + "\"", filters)
+			properties = MasterUnSoldProperty.findByAddress("\"" + params.address + "\"", filters)
 		}
 
-		printf("A")
 		return properties
+	}
+
+	def getUser(params) {
+		def user
+
+		if(params.email) {
+			user = User.findByEmail(params.email)
+		}
+		else if(params.uid) {
+			user = User.findById(params.uid)
+		}
+
+		return user
+	}
+
+	def addToWatchList(params){
+		def user
+		def property
+
+		
+		if(params.uid){
+			user = User.findById(params.uid)
+		}else if(params.email) {
+			user = User.findByEmail(params.email)
+		}
+
+		if(user && params.pid) {
+			property = MasterUnSoldProperty.findById(params.pid)
+			if(property) {
+				//fix issue here
+				//user.addToProperties(property).save()
+			}
+		}
+
+		return user
 	}
 }
