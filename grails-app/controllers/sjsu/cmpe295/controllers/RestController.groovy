3af8317bc2051fabd4b08e0f0bf1d3b09a025ff1
dmellosnehal23 <snehal.v.dmello@gmail.com>
2014-04-14 23:44:50 -0700

    Added Rest Controller

---
+++ grails-app/controllers/sjsu/cmpe295/controllers/RestController.groovy
@@ -0,0 +1,19 @@
+package sjsu.cmpe295.controllers
+
+import sjsu.cmpe295.models.MasterUnSoldProperty
+
+class RestController {
+	def masterUnSoldProperty;
+	
+	//def address = params.address
+	//def radius = params.radius
+	/*def city = params.city
+
+    def index() { }
+	
+	def getUnsoldProperties() {
+		printf("In RestController/getUnsoldProperties")
+		MasterUnSoldProperty masterUnSoldProperty = MasterUnSoldProperty.findByCity(city)
+		return masterUnSoldProperty
+	}*/
+}
